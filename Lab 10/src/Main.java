a24f05ea7d52b0b2d7bfc533220220125b20339f
nsikan.akpan <nsikan.akpan@betmakers.com>
2023-07-01 14:56:37 -0400

    finished lab 10

--- Lab 10/src/Main.java
+++ Lab 10/src/Main.java
@@ -1,5 +1,43 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+  public static void main(String[] args) {
+    try {
+      Scanner scan = new Scanner(System.in);
+
+      System.out.println("Please enter the first file name");
+      String file1 = scan.nextLine();
+
+      System.out.println("Please enter the second file name");
+      String file2 = scan.nextLine();
+
+      File myFile1 = new File(file1);
+      File myFile2 = new File(file2);
+
+      Scanner file1Scan = new Scanner(myFile1);
+      Scanner file2Scan = new Scanner(myFile2);
+
+      String line1 = "";
+      String line2 = "";
+
+      int lineCount = 1;
+      while (file1Scan.hasNextLine() && file2Scan.hasNextLine()) {
+        line1 = file1Scan.nextLine();
+        line2 = file2Scan.nextLine();
+        if (!line1.equals(line2)) {
+          System.out.println("\nLine " + lineCount);
+          System.out.println("< " + line1);
+          System.out.println("> " + line2);
+        }
+        lineCount++;
+      }
+      if (!file1Scan.hasNextLine() || !file2Scan.hasNextLine()) {
+        System.out.println("\nFiles have a different number of lines\n");
+      }
+    } catch (Exception err) {
+      System.out.println(err);
     }
+  }
 }
