ed7efec2c37f140b63b5227834006898b3f3cf2b
nsikan.akpan <nsikan.akpan@betmakers.com>
2023-07-02 10:40:34 -0400

    assing 7 inital

---
+++ Assignment 7/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+  private int[] quizArr = new int[10];
+  private int[] homeworkArr = new int[10];
+  private int midtermGrade;
+  private int finalExamGrade;
+  private double quizAverage;
+  private double homeworkAverage;
+  
+
+}
