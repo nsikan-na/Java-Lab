32ea8cfb04e0c6e42cf9886d2ff67a0e0b0a60c2
Nsikan Akpan <nsikan.akpan@betmakers.com>
2023-07-14 22:14:24 -0400

    fixed assign 7

--- M8 - Data Structures/Assignment 7/src/Main.java
+++ M8 - Data Structures/Assignment 7/src/Main.java
@@ -5,12 +5,12 @@ public class Main {
   public static void main(String[] args) {
     try {
       StatisticGradeBook statisticGradeBook = new StatisticGradeBook(
-        "Assignment7-Spreadsheet.csv"
+              "Assignment7-Spreadsheet.csv"
       );
       Scanner scan = new Scanner(System.in);
-      Thread myThread = new Thread();
+      Thread myThread = new Thread(statisticGradeBook);
       myThread.start();
-      System.out.println("Please enter a students name");
+      System.out.println("What student would you like to see grades for");
       String name = scan.nextLine();
       statisticGradeBook.getStudentGrade(name);
       scan.close();
